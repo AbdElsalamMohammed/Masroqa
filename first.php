c3a3937bae9d5885bb58c232d15a669722cfd8a3
AbdElsalam Mohamed <abdelsalammohamed@stud.fci-cu.edu.eg>
2018-12-21 21:42:57 +0200

    First Page With ADD & BROWSE buttons

---
+++ first.php
@@ -0,0 +1,21 @@
+<?php ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Registration</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>    
+    <form class="search" method="post" action="Search_.php">
+    <p>
+        <button type="submit" class="btn" name="search__">Browse</button>
+    </p>
+    </form>
+    
+    <form class="upload" method="post" action="Upload_.php">
+    <p>
+        <button type="submit" class="btn" name="upload__">Add Item</button>
+    </p>
+    </form>
+    
+</body>
