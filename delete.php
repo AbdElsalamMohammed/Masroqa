be48eb629fedbdb007f5be095faf69e7763f4878
AbdElsalam Mohamed <abdelsalammohamed@stud.fci-cu.edu.eg>
2018-12-22 21:45:36 +0200

    Add "delete" function php code

--- delete.php
+++ delete.php
@@ -1,4 +1,42 @@
+<?php
+// variable declaration
+$errors = array(); 
+session_start();
+$db = mysqli_connect('localhost', 'root', '11111111', 'item');
 
+if (isset($_POST['verfiy_password'])) {
+	// receive input value from the form
+	$password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
+    
+	// form validation: ensure that the form is correctly filled
+	if (empty($password_1)) { array_push($errors, "Password is required"); }
+    $password_1 = md5($password_1);
+    // variable declaration
+    $_SESSION['success'] = "";
+    $id=$_GET['id2'];
+    $pass=$_GET['Pass'];
+    $q=false;
+	// delete item if there are no errors in the form
+	if (count($errors) == 0) {
+		if ($pass == $password_1)
+        {
+            $con=mysql_connect("localhost","root","11111111");
+            mysql_select_db("item");
+            $q=mysql_query("delete from item where id=$id");
+        }
+        if ($q)
+        {
+            header("location:browse.php");
+        }
+        else
+        {
+            echo "Not Deleted";
+        }
+		$_SESSION['success'] = "Deleted";
+		//header('location: browse.php');
+	}
+}
+?>
 <!DOCTYPE html>
 <html>
 <head>
